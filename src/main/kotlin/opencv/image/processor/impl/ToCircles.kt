4ca0b4397adc278ed9b44c6ec2139132f072e92c
pjaro <p.jaronsky@gmail.com>
2020-12-02 19:10:15 +0100

    Cosmetic changes

--- src/main/kotlin/opencv/image/processor/impl/ToCircles.kt
+++ src/main/kotlin/opencv/image/processor/impl/ToCircles.kt
@@ -1,7 +1,8 @@
 package opencv.image.processor.impl
 
+import opencv.image.processor.api.AbstractImageProcessor
 import org.opencv.imgproc.Imgproc
 
 object ToCircles {
-    operator fun invoke(radius: Double) = ToKernel(Imgproc.MORPH_ELLIPSE, radius, radius)
+    operator fun invoke(radius: Double): AbstractImageProcessor = ToKernel(Imgproc.MORPH_ELLIPSE, radius, radius)
 }
